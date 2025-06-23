fe4c8d5b8bfa339967b08bf154e1b7aea991d209
Islam <mislam41@kent.edu>
2025-06-23 17:00:05 -0400

    training stage seems okay

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 import argparse
 from src.utils.common_utils import read_yaml
 from src.utils.data_mgmt import get_data
+from src.utils.model import create_model
 
 
 def training(config_path):
@@ -9,6 +10,16 @@ def training(config_path):
     validation_datasize = config['params']['VALIDATION_DATASIZE']
     print(f" {validation_datasize}")
     (X_train, y_train),(X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
+    LOSS_FUNCTION = config['params']['LOSS_FUNCTION']
+    OPTIMIZER = config['params']['OPTIMIZER']
+    METRICS = config['params']['METRICS']
+    NUM_CLASSES = config['params']['NUM_CLASSES']
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+    EPOCHS = config['params']['EPOCHS']
+    VALIDATION  = (X_valid, y_valid)
+
+    history = model.fit(X_train, y_train, epochs = EPOCHS, validation_data = VALIDATION)
+    
 
 
 
