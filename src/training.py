db6ea3dd38016dc7f282bf39a9e92e88d1a4a51c
Islam <mislam41@kent.edu>
2025-07-03 11:04:01 -0400

    Logger implementated

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import argparse
+import logging
 from src.utils.common_utils import read_yaml
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model, save_plot
@@ -8,10 +9,10 @@ from src.utils.callbacks import get_callbacks
 
 
 def training(config_path):
+
     config = read_yaml(config_path)
-    #print(config)
+    logging.info(">>> Data Loading and splitting started>>>>")
     validation_datasize = config['params']['VALIDATION_DATASIZE']
-    print(f" {validation_datasize}")
     (X_train, y_train),(X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
     LOSS_FUNCTION = config['params']['LOSS_FUNCTION']
     OPTIMIZER = config['params']['OPTIMIZER']
@@ -20,7 +21,9 @@ def training(config_path):
     model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
     EPOCHS = config['params']['EPOCHS']
     VALIDATION  = (X_valid, y_valid)
+    logging.info(">>> Data Loading and splitting completed>>>>")
 
+    
     CALLBACK_LIST = get_callbacks(config, X_train)
 
     history = model.fit(X_train, y_train, epochs = EPOCHS, validation_data = VALIDATION, callbacks = CALLBACK_LIST)
@@ -46,6 +49,18 @@ def training(config_path):
 
 
 if __name__ == '__main__':
+
+    #logging
+    logging_str = "[%(asctime)s :  %(levelname)s : %(module)s] : %(message)s"
+    log_dir = "logs/general_logs"
+    os.makedirs(log_dir, exist_ok=True)  # Create log directory if it doesn't exist
+    logging.basicConfig(filename=os.path.join(log_dir, 'running_log.log'),
+    filemode='a',  # 'w' to overwrite the log file, 'a' to append
+    format=logging_str,
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(">>> General logging started >>>>")
+
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="config/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -7,7 +7,7 @@ import json
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
-    #logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
     return content
 
 def create_directories(path_to_directories: list) -> None:
